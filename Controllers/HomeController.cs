50533a51151d26de2a0c4de7118faaee89f6f406
Nardos Lengo <nardi.8199@gmail.com>
2021-06-12 23:01:56 -0700

    Dojo Survey MVCII commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/survey")]
+        public IActionResult Submission(Survey yourSurvey)
+        {
+            return View("Show",yourSurvey);
+        }
+
+    }
+}
